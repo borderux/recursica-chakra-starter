870cfda48e84240a8dd355132efcebdfd90fd68d
Matt Massey <matt@borderux.com>
2021-02-10 23:33:20 -0800

    Added Chakra button and sample app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Recursica meet Chakra</h1>
+      <p>
+        Chakra UI is an excellent design system framework. This samples shows
+        how you can easily update/modify Chakra with the Recursica design
+        system.
+      </p>
+      <h2>Components</h2>
+      <Button colorScheme={"blue"}>Chakra Button</Button>
+      <h2>Theme Colors</h2>
+      <h2>Theme Typography</h2>
+    </>
   );
 }
 
